3485ed189123c26165afcfafe84b2373ab4d1113
vincentole <63076449+vincentole@users.noreply.github.com>
2024-01-03 19:44:44 +0100

    update config

--- config.fish
+++ config.fish
@@ -8,10 +8,18 @@ alias ll 'exa -lbah --git'
 alias bat batcat
 set -g -x BAT_THEME gruvbox-dark
 
+
+# Node version manager: https://github.com/Schniz/fnm
+# Make sure the fnm bin is in the path
+fnm env | source
+
 # Set llvm to version 14 - Odin-lang requires this currently
 #set -g -x LLVM_CONFIG /usr/bin/llvm-config-14
 #alias llvm-config llvm-config-14
 #fish_add_path /home/ole-urfels/.local/share/Odin/
+
+fish_add_path ~/.cargo/bin/
+fish_add_path ~/.cargo/env
 
 function rmnvim
     rm -rf ~/.config/nvim
@@ -20,4 +28,21 @@ function rmnvim
     rm -rf ~/.local/state/nvim
     rm -rf ~/.cache/nvim
     printf "Deleted the following directories:\n~/.config/nvim\n~/.local/share/nvim\n~/.local/state/nvim\n~/.cache/nvim\n\nCreated the following directory:\n~/.config/nvim"
+end
+
+function pushdots
+    function add_and_push
+        git add .
+        git commit -m 'update config'
+        git push
+    end
+    
+    cd ~/.config/nvim/
+    add_and_push
+
+    cd ~/.config/kitty/
+    add_and_push
+
+    cd ~/.config/fish/
+    add_and_push
 end
